9185d073f456ccff90ce75330d11fe8f7402bfec
surh <surh@users.noreply.github.com>
2016-04-05 17:33:02 -0400

    Adding summary.PCO and print.summary.PCO

--- R/PCO.r
+++ R/PCO.r
@@ -18,3 +18,32 @@ PCO.Dataset <- function(Dat,dim=3,distfun=dist){
   mat.pco$Tax <- Dat$Tax
   return(mat.pco)
 }
+
+summary.PCO <- function(object){
+  object <- Dat.pco
+  ncomponents <- ncol(object$points)
+  components <- paste("PCo",1:ncomponents,sep = "")
+  percvar <- 100*(object$eig[ object$eig >= 0 ]) / sum(object$eig[ object$eig >= 0 ])
+  cumvar <- cumsum(percvar)
+  
+  vartab <- data.frame(Component = components,
+                       Var.explained = percvar[1:ncomponents],
+                       Cumulative = cumvar[1:ncomponents])
+  
+  sum.pco <- list(vartab = vartab,
+                  ncomponents = ncomponents)
+  class(sum.pco) <- "summary.PCO"
+  
+  return(sum.pco)
+}
+
+print.summary.PCO <- function(x,digits = 2, n = 5){
+  cat("Principal Coordinate Analysis:\n")
+  cat("\t",x$ncomponents, " Components\n\n")
+  
+  tab <- x$vartab[ 1:min(n,nrow(x$vartab)), ]
+  tab[ ,2:3 ] <- round(tab[,2:3],digits = digits)
+  print(tab)
+}
+
+
