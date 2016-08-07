772c6cb01d1b0186cbc6d2b7f7214a2cf2d7b647
surh <surh@users.noreply.github.com>
2016-08-07 17:00:52 -0400

    measurable_taxa function that replaces the findGoodOTUs method.

---
+++ R/measurable_taxa.r
@@ -0,0 +1,67 @@
+#' Identify measurable taxa
+#' 
+#' Identfies taxa above certain abundance and prevalence
+#' thresholds, that can be quantitatively analyzed
+#' 
+#' @aliases findGoodOTUs
+#' 
+#' @param Tab Count table matrix. Samples as columns and OTUs as rows.
+#' @param Dat A \code{Dataset} object.
+#' @param method Either "absolute" (min absolute number of reads in min
+#' number of sample) or "AbsProp" (absolute number if reads and proportion
+#' of samples).
+#' @param table logical indicating whether an abundance table should be
+#' returned
+#' @param clean logical, indicating whether the function \code{\link{clean}}
+#' should be applied to the result.
+#' 
+#' @return When table = FALSE, it returns a logical index vector that shows which taxa pass the
+#' thresholds.
+#' 
+#' When table = TRUE, it returns a matrix or Dataset object (depending on the method used),
+#' where samples that do not pass the thresholds have been removed
+#' 
+#' @author Sur from Dangl Lab.
+measurable_taxa <- function(...) UseMethod("good_taxa")
+
+measurable_taxa.default <- function(Tab,min_reads_otu,min_samples_otu,method="absolute",
+                              table = TRUE){
+  if(method == "absolute"){
+    #Count samples where OTU is above threshold
+    samples_per_otu <- rowSums(Tab >= min_reads_otu)
+    index <- samples_per_otu >= min_samples_otu
+    #return(index)
+  }else if(method == "AbsProp"){
+    if(min_samples_otu < 0 || min_samples_otu > 1){
+      stop("findGoodOTUs: Invalid proportion of samples. Must be in the range [0,1]",call.=TRUE)
+    }
+    reads_per_otu <- rowSums(Tab)
+    #Count samples in which OTU appears
+    samples_per_otu <- rowSums(Tab > 0)
+    #Convert proportion of samples to absolute number
+    min_samples_otu <- min_samples_otu*dim(Tab)[2]
+    print(min_samples_otu)
+    index1 <- samples_per_otu >= min_samples_otu
+    index2 <- reads_per_otu >= min_reads_otu
+    index <- index1 & index2
+    #return(index)
+  }else{
+    stop("findGoodOTUs: Invalid method",call.=TRUE)
+  }  
+  
+  if(table){
+    Tab <- Tab[index,]
+    return(Tab)
+  }else{
+    return(index)
+  }
+}
+
+
+
+
+findGoodOTUs <- function(...){
+  warn("This function will be deprecated soon. Use measurable_taxa instead\n")
+  good_taxa(...)
+}
+
